78d4f620c227b22a48e898df102b822f2be9997b
Skaven Lover <sexyskavenlover@gmail.com>
2021-12-24 16:00:17 -0300

    agregado trabajo

--- index.js
+++ index.js
@@ -10,8 +10,29 @@ class Usuario{
     getFullName(){
         return `El nombre es ${this.nombre} ${this.apellido}` 
     }
+    addMascota(nuevaMascota){
+        this.mascotas.push(nuevaMascota);
+    }
+    countMascotas(){
+        return this.mascotas.length
+    }
+    addBook(titulo, autor){
+        this.libros.push({ titulo: titulo, autor: autor }) 
+}
+    getBookNames(){
+    return this.libros.forEach(x => console.log(x.titulo))
+    }
 }
 
-let manolo = new Usuario ('Manolo','El del Puerto', [{nombre:'harry potter', autor:'jkrowling'}, {nombre:'el señor de los anillos', autor:'tolkien'}], ['perro','gato','cobra','oso panda'] );
+let manolo = new Usuario ('Manolo','El del Puerto', [{titulo:'harry potter', autor:'jkrowling'}, {titulo:'el señor de los anillos', autor:'tolkien'}], ['perro','gato','cobra','oso panda'] );
+
+
+
 console.log(manolo.getFullName())
 console.log(manolo)
+manolo.addMascota("conejo al escabeche")
+console.log(manolo.mascotas)
+console.log(manolo.countMascotas())
+manolo.addBook("El arte de la guerra", "Sun Tzu")
+console.log(manolo.libros)
+manolo.getBookNames()
